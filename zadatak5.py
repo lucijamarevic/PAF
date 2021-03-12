4689ab10287d1c2c311dc648b82b306e48b6a7b2
lucijamarevic <80029240+lucijamarevic@users.noreply.github.com>
2021-03-12 16:24:23 +0100

    Update zadatak5.py

--- zadatak5.py
+++ zadatak5.py
@@ -11,7 +11,12 @@ def nadi_jednadzbu(x1,x2,y1,y2):
 def nacrtaj_graf(x1,x2,y1,y2):
     x = [x1,x2]
     y = [y1,y2]
+    osi = plt.axes()
+    osi.set_xlim([min(x)-1,max(x)+1])
+    osi.set_ylim([min(y)-1,max(y)+1])
     plt.plot(x,y)
+    plt.scatter(x1,y1, s=100, c="green")
+    plt.scatter(x2,y2, s=100, c="red")
     pitanje = input("Zelite li  graf odmah (odgovorite 'odmah') ili u pdf-u (odgovorite 'PDF')? ")
     if pitanje == "odmah":
         plt.show()
