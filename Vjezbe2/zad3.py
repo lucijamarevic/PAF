554dfb32469b19d2188c883d87794b64e7cddbd7
lucijamarevic <80029240+lucijamarevic@users.noreply.github.com>
2021-03-18 23:27:12 +0100

    Adding zad3.py

---
+++ Vjezbe2/zad3.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import math
+from kinematika import jednoliko_gibanje, kosi_hitac
+
+jednoliko_gibanje(10,1,50)
+kosi_hitac(50,60,20)
+
+#while True:
+    #sila = input("Unesite iznos sile u Newtonima: ")
+    #try:
+        #F = float(sila)
+        #break
+    #except ValueError:
+        #print("Iznos sile mora biti broj, ponovite unos!")
+
+#while True:
+    #masa = input("Unesite masu u kilogramima: ")
+    #try:
+        #m = float(masa)
+        #break
+    #except ValueError:
+        #print("Masa mora biti broj, ponovite unos!")
+
+#while True:
+    #p_b = input("Unesite iznos pocetne brzine u metrima po sekundi: ")
+    #try:
+        #v0 = float(p_b)
+        #break
+    #except ValueError:
+        #print("Iznos pocetne brzine mora biti broj, ponovite unos!")
+
+#while True:
+    #k_o = input("Unesite iznos kuta otklona pocetne brzine u stupnjevima: ")
+    #try:
+        #kut = float(k_o)
+        #th = math.pi*kut/180
+        #break
+    #except ValueError:
+        #print("Iznos kuta otklona mora biti broj, ponovite unos!")
+
+#while True:
+    #vrijeme = input("Unesite vrijeme kroz koje se odvija gibanje u sekundama: ")
+    #try:
+        #t = float(vrijeme)
+        #break
+    #except ValueError:
+        #print("Vrijeme mora biti broj, ponovite unos!")
