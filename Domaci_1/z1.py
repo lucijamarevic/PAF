2bf9d4eff30b4f777fe02d911575246cb2daa4d4
lucijamarevic <80029240+lucijamarevic@users.noreply.github.com>
2021-04-01 23:24:24 +0200

    Delete z1.py

--- Domaci_1/z1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import math
-import particle as prt
-
-p1 = prt.Particle()
-
-p1.init(10,60,0,0,0.01)
-print("Ukupno vrijeme trajanja gibanje iznosi: {:.2f}s".format(p1.total_time()))
-print("Najveca ostvarena brzina iznosi: {:.2f}m/s".format(p1.max_speed()))
