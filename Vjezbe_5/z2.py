07f4d8bfd684dd0e4df827f520072c4c45dc2cea
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-08 21:58:49 +0200

    Vjezbe_5

--- Vjezbe_5/z2.py
+++ Vjezbe_5/z2.py
@@ -6,5 +6,3 @@ h1 = ho.HarmonicOscillator()
 h1.init(0.1,2,0,0.5,0.01)
 print("Period titranja iznosi: {:.2f}".format(h1.period()))
 print("Analiticki period titranja iznosi: {:.2f}".format(h1.period_analitic()))
-
-print("Pogreska je: ")
