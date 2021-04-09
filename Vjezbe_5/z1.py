5ea1ec31c7515bb7fea8b03b73fc6c25a3a54d7e
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-09 21:38:38 +0200

    Vjezbe_5

--- Vjezbe_5/z1.py
+++ Vjezbe_5/z1.py
@@ -1,26 +1,25 @@
-## dodaj komentare za init funkciju
 import harmonic_oscillator as ho 
 import matplotlib.pyplot as plt
 
 h1 = ho.HarmonicOscillator()
 
-h1.init(0.1,5,0,0.3,0.01)
+h1.init(0.1,5,0,0.3,0.01)    #(m,k,v0,A,dt)
 h1.oscillate(2)
 h1.plot_trajectory()
 plt.scatter(h1.t,h1.x, s = 2, c = "green", label = "dt = 0.01")
 h1.reset()
 
-h1.init(0.1,5,0,0.3,0.025)
+h1.init(0.1,5,0,0.3,0.025)    #(m,k,v0,A,dt)
 h1.oscillate(2)
 plt.scatter(h1.t,h1.x, s = 4, c = "orange", label = "dt = 0.025")
 h1.reset()
 
-h1.init(0.1,5,0,0.3,0.05)
+h1.init(0.1,5,0,0.3,0.05)    #(m,k,v0,A,dt)
 h1.oscillate(2)
 plt.scatter(h1.t,h1.x, s = 6, label = "dt = 0.05")
 h1.reset()
 
-h1.init(0.1,5,0,0.3,0.01)
+h1.init(0.1,5,0,0.3,0.01)    #(m,k,v0,A,dt)
 h1.analitic_x(2)
 plt.plot(h1.t, h1.x, c = "red", label = "analitic")
 h1.reset()
