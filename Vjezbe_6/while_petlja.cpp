e986ea8bd7f8c5e8c478cfbc56ef42bfc8422a15
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-14 23:07:24 +0200

    probne petlje

---
+++ Vjezbe_6/while_petlja.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//using namespace std;
+
+int main() {
+    int i = 1; 
+
+    // while loop from 1 to 5
+    while (i <= 5) {
+        std::cout << i << " ";
+        ++i;
+    }
+    
+    return 0;
+}
