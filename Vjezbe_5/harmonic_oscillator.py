5033fec20594ff6fc1dcb38ca1dcd1b78d5408d0
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-15 21:17:31 +0200

    Vjezbe_5

--- Vjezbe_5/harmonic_oscillator.py
+++ Vjezbe_5/harmonic_oscillator.py
@@ -87,18 +87,29 @@ class HarmonicOscillator:
             
         return self.x, self.t
     
-        ## radi, ali izbacuje krivi rezultat, sutra cu to pogledat
-    #def period(self):
-    #    while True:
-    #        self.__move()
-    #        if self.xi <= self.x0 + self.dt and self.xi >= self.x0 - self.dt:
-    #            T = 2*self.ti       
-    #            break
+    def period(self):               # izbacuje krivi rezultat
+        while True:
+            self.__move()
+            if self.xi <= self.x0 + self.dt and self.xi >= self.x0 - self.dt:
+                T = 2*self.ti       
+                break
+        return T
 
-    #    return T
+    def period_analitic(self):
+        return 2*math.pi*math.sqrt(self.m/self.k)
 
-        ## takoder radi, ali izbacuje krivi rezultat
-    #def period(self):
+    #def period(self):              # izbacuje krivi rezultat
+    #    self.oscillate(10)
+    #    del self.x[0]
+    #    del self.t[0]
+    #    for e in self.x:
+    #        if e <= self.x0 + self.dt and e >= self.x0 - self.dt:
+    #            i = self.x.index(e)
+    #            ti = self.t[i]
+    #            break        
+    #    return 2*ti
+
+    #def period(self):              # izbacuje krivi rezultat
     #    t = 0
     #    while True:
     #        self.oscillate(t)
@@ -107,21 +118,4 @@ class HarmonicOscillator:
     #            break
     #        else:
     #            t+=self.dt  
-
     #    return T
-
-        ## takoder radi, ali izbacuje krivi rezultat
-    def period(self):
-        self.oscillate(10)
-        del self.x[0]
-        del self.t[0]
-        for e in self.x:
-            if e <= self.x0 + self.dt and e >= self.x0 - self.dt:
-                i = self.x.index(e)
-                ti = self.t[i]
-                break        
-
-        return 2*ti
-
-    def period_analitic(self):
-        return 2*math.pi*math.sqrt(self.m/self.k)
