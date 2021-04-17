f6635afc953c8312f2496710afc71d15330c4594
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-18 01:33:29 +0200

    nezavrseno

--- Domaci_2/modul.py
+++ Domaci_2/modul.py
@@ -1,16 +1,79 @@
 import matplotlib.pyplot as plt 
 
 class Particle:
-    def init(self,f,x0,v0,total_time,dt):
-        self.f = f
-        self.x0 = x0
-        self.v0 = v0
+    def init(self,func,m,x0,v0,total_time,dt):
+        self.func = func
+        self.m = m
+        self.xi = x0
+        self.vi = v0
+        self.ti = 0
+        self.fi = self.func(self.vi, self.xi, self.ti)
+        self.ai = self.fi/self.m
         self.t_uk = total_time
         self.dt = dt
-
+        self.t = []
+        self.f = []
+        self.a = []
+        self.v = []
+        self.x = []
+        self.t.append(self.ti)
+        self.f.append(self.fi)
+        self.a.append(self.ai)
+        self.v.append(self.vi)
+        self.x.append(self.xi)
+    
     def reset(self):
-        del self.f
-        del self.x0
-        del self.v0
+        del self.F
+        del self.m
+        del self.ai
+        del self.xi
+        del self.vi
+        del self.ti
         del self.t_uk
         del self.dt
+        del self.t
+        del self.a
+        del self.v
+        del self.x
+
+    def move(self):
+        while self.ti <= self.t_uk:            
+            self.ti += self.dt
+            self.vi = self.vi + self.ai * self.dt
+            self.xi = self.xi + self.vi * self.dt
+            self.fi = self.func(self.vi,self.xi,self.ti)
+            self.ai = self.fi/self.m
+            self.t.append(self.ti)
+            self.v.append(self.vi)
+            self.x.append(self.xi)
+            self.f.append(self.fi)
+            self.a.append(self.ai)
+
+        return self.x, self.v, self.a, self.t
+
+    def plot_trajectory(self):
+        self.move()
+        plt.figure("Movement description")
+        fig = plt.subplot()
+        plt.subplot(2,2,1)
+        plt.plot(self.t,self.x)
+        plt.xlabel("t [s]")
+        plt.ylabel("x [m]")
+        plt.title("x-t graf")
+        plt.subplot(2,2,2)
+        plt.plot(self.t,self.v)
+        plt.xlabel("t [s]")
+        plt.ylabel("v [m/s]")
+        plt.title("v-t graf")
+        plt.subplot(2,2,3)
+        plt.plot(self.t,self.a)
+        plt.xlabel("t [s]")
+        plt.ylabel("a [m/s^2]")
+        plt.title("a-t graf")
+        plt.subplot(2,2,4)
+        plt.plot(self.t,self.f)
+        plt.xlabel("t [s]")
+        plt.ylabel("F [N]")
+        plt.title("F-t graf")
+        plt.subplots_adjust(wspace = 0.4, hspace = 0.6)
+        plt.show()
--- Domaci_2/provjera.py
+++ Domaci_2/provjera.py
@@ -1,10 +1,18 @@
+# modul radi za bilo koju silu koju korisnik moze definirati kao funkciju brzine, vremena i polozaja
 import modul as m
 
-def f(x,k):
+p1 = m.Particle()
+p2 = m.Particle()
+
+def f1(v,x,t):
+    return 10
+
+p1.init(f1,1,0,0,1,0.01)    #(func,m,x0,v0,total_time,dt)
+p1.plot_trajectory()
+
+def f2(v,x,t):
+    k = 10
     return -k*x
 
-p1 = m.Particle()
-for x in range(1,11):
-    p1.init(f(x,10),0,0,10,0.01)     # (f,x0,v0,total_time,dt)
-    print(p1.f)
-    p1.reset()
+p2.init(f2,1,0,0,5,0.01)    #(func,m,x0,v0,total_time,dt)
+p2.plot_trajectory()
