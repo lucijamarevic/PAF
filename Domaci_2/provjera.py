390b8a7752827339020446a818a7e6c58ea6e6d2
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-18 02:30:43 +0200

    mozda dovrseno

--- Domaci_2/provjera.py
+++ Domaci_2/provjera.py
@@ -1,12 +1,10 @@
 # modul radi za bilo koju silu koju korisnik moze definirati kao funkciju brzine, vremena i polozaja
 import modul as m
 
-p1 = m.Particle()
-p2 = m.Particle()
-
 def f1(v,x,t):
     return 10
 
+p1 = m.Particle()
 p1.init(f1,1,0,0,1,0.01)    #(func,m,x0,v0,total_time,dt)
 p1.plot_trajectory()
 
@@ -14,5 +12,6 @@ def f2(v,x,t):
     k = 10
     return -k*x
 
-p2.init(f2,1,0,0,5,0.01)    #(func,m,x0,v0,total_time,dt)
+p2 = m.Particle()
+p2.init(f2,1,2,0,5,0.01)    #(func,m,x0,v0,total_time,dt)
 p2.plot_trajectory()
