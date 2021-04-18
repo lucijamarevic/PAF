61e87d1d6ecfb5da727d61d522c167487f47b68e
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-18 12:49:40 +0200

    Domaci_2

--- Domaci_2/provjera.py
+++ Domaci_2/provjera.py
@@ -1,11 +1,12 @@
 # modul radi za bilo koju silu koju korisnik moze definirati kao funkciju brzine, vremena i polozaja
+import matplotlib.pyplot as plt
 import modul as m
 
 def f1(v,x,t):
     return 10
 
 p1 = m.Particle()
-p1.init(f1,1,0,0,1,0.01)    #(func,m,x0,v0,total_time,dt)
+p1.init(f1,1.5,0,0,1,0.01)    #(func,m,x0,v0,total_time,dt)
 p1.plot_trajectory()
 
 def f2(v,x,t):
@@ -13,5 +14,31 @@ def f2(v,x,t):
     return -k*x
 
 p2 = m.Particle()
-p2.init(f2,1,2,0,5,0.01)    #(func,m,x0,v0,total_time,dt)
+p2.init(f2,1.5,2,0,5,0.01)    #(func,m,x0,v0,total_time,dt)
 p2.plot_trajectory()
+
+# cisto iz znatizelje i radi dodatne provjere
+def f3(v,x,t):     
+    a = 1
+    return a*v*v
+
+p3 = m.Particle()
+p3.init(f3,2,0,1,1,0.01)    #(func,m,x0,v0,total_time,dt)
+#p3.plot_trajectory()
+
+def f4(v,x,t):     
+    b = 3
+    return b*t
+
+p4 = m.Particle()
+p4.init(f4,2,0,0,2,0.01)    #(func,m,x0,v0,total_time,dt)
+#p4.plot_trajectory()
+
+def f5(v,x,t):     
+    a = 3
+    b = 2
+    return a*x + v*t*t
+
+p5 = m.Particle()
+p5.init(f5,5,1,1,2,0.01)    #(func,m,x0,v0,total_time,dt)
+#p5.plot_trajectory()
