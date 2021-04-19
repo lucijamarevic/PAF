a7cd8b24b87347cff7057bcb4a6cdf6ff16dd6fd
Lucija <lucijamarevic@gmail.com>
2021-04-19 12:11:09 +0200

    Kolokvij

---
+++ Kolokvij/vertikalni_hitac.py
@@ -0,0 +1,8 @@
+
+class VertikalniHitac:
+    def init(self,h0,v0):
+        self.h0 = h0
+        self.v0 = v0
+        
+        print("Objekt je uspjesno storen.")
+        print("Pocetna visina iznosi: {:.2f}m, a pocetna brzina: {:.2f}m/s".format(h0,v0))
---
+++ Kolokvij/zadatak_1.py
@@ -0,0 +1,4 @@
+import vertikalni_hitac as vh 
+
+p1 = vh.VertikalniHitac()
+p1.init(2.345,1.563)    #(h0,v0)
