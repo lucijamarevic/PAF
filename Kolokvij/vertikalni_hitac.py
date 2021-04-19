e6a85c29764ba93af392310f478ec78af2387ba0
Lucija <lucijamarevic@gmail.com>
2021-04-19 12:20:17 +0200

    Kolokvij

--- Kolokvij/vertikalni_hitac.py
+++ Kolokvij/vertikalni_hitac.py
@@ -6,3 +6,11 @@ class VertikalniHitac:
         
         print("Objekt je uspjesno storen.")
         print("Pocetna visina iznosi: {:.2f}m, a pocetna brzina: {:.2f}m/s".format(h0,v0))
+
+    def change_height(self,novi_h0):
+        self.h0 = novi_h0
+        print("Nova pocetna visina iznosi: {:.2f}m".format(self.h0))
+
+    def change_speed(self,novi_v0):
+        self.v0 = novi_v0
+        print("Nova pocetna brzina iznosi: {:.2f}m/s".format(self.v0))
---
+++ Kolokvij/zadatak_2.py
@@ -0,0 +1,11 @@
+import vertikalni_hitac as vh 
+
+p1 = vh.VertikalniHitac()
+p1.init(2,1)    #(h0,v0)
+p1.change_height(3)
+p1.change_speed(0.5)
+
+p2 = vh.VertikalniHitac()
+p2.init(4,1.5)    #(h0,v0)
+p2.change_height(2.5)
+p2.change_speed(3.1)
