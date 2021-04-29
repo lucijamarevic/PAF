a0887d2d91224fe3549f6b5b9456835e0a1226c6
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-29 02:01:15 +0200

    nedovrseno

--- Vjezbe_6/z3.cpp
+++ Vjezbe_6/z3.cpp
@@ -4,22 +4,33 @@
 using namespace std;
 
 const int n = 5;
-int lista[n] = {1,2,3,4,5};
+int lista[n] = {1,5,2,4,6};
 
 void ispis_u_intervalu(unsigned a, unsigned b) {
     for(int i = 0; i < n; i++) {
-        if (lista[i] < a && lista[i] < b) {
-            cout << lista[i] << endl;
+        if (lista[i] >= a && lista[i] <= b) {
+            cout << lista[i] << " ";
         }
         else {
             continue;
+        } } 
+        cout << endl;
+        }   
+
+void reverse_list()  {   // ne valja
+    cout << "Obrnuti redosljed glasi: " << endl;
+    int i,j,element;
+    for (j = n - 1; j = 0; j--) {
+        element = lista[j];
+        for (i = 1; i < n; i++) {
+        lista[i] = element;
         }
+        cout << lista[i] << endl;
     }
 }
 
-// izbacuje ovo
-
-/* C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/
-../lib/gcc/x86_64-w64-mingw32/8.1.0/../../../../x86_64-w64-mingw32/lib/../lib/
-libmingw32.a(lib64_libmingw32_a-crt0_c.o):crt0_c.c:(.text.startup+0x2e): 
-undefined reference to `WinMain'collect2.exe: error: ld returned 1 exit status */
+int main() {
+    ispis_u_intervalu(2,7);
+    reverse_list();
+    return 0;
+}
