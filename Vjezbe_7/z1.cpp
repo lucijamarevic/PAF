5698ccb8ea147b0cdc2e5ee55e4e9542546ca61a
Lucija <lucijamarevic@gmail.com>
2021-04-29 09:30:47 +0200

    provjerit jel radi

--- Vjezbe_7/z1.cpp
+++ Vjezbe_7/z1.cpp
@@ -9,9 +9,9 @@ class Particle {
         double ti,xi,yi,vx,vy,kut;    //attributes
         double dt;
         double g = -9.81;
+        double D,t;
 
         Particle(double v0, double theta, double x0, double y0, double step = 0.001)  //constructor
-        //~Particle();
         {
             ti = 0;
             dt = step;
@@ -22,8 +22,27 @@ class Particle {
             vy = v0*sin(kut);
         }
 
-        double range();
-        double time();
+        double range()
+            {
+                D = yi;
+                while (true) {
+                    evolve();
+                    if (yi > D) {
+                        D = yi;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+            return D;
+            }
+
+        double time()
+        {
+            evolve();
+            t = ti;
+            return t;
+        }
     
     private:
 
@@ -40,4 +59,11 @@ class Particle {
             }
         }
 
-};
+};  //end of class
+
+int main() {
+    Particle p1(10,60,0,0);  // creating an object, step by default (v0,theta,x0,y0,step) 
+    std::cout << "Domet iznosi: " << p1.range() << "m." << std::endl;  // calling a method
+    std::cout << "vrijeme iznosi: " << p1.time() << "s." << std::endl;  // calling a method
+    return 0;
+    }
