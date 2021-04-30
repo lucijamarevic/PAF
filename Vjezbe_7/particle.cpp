e63b5c27682d9ecfea232eaa388202f6b0643cbc
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-30 15:52:45 +0200

    neki error

--- Vjezbe_7/particle.cpp
+++ Vjezbe_7/particle.cpp
@@ -1,59 +1,44 @@
 #include <iostream>
 #define _USE_MATH_DEFINES
 #include <math.h>
-#include <particle.h>
+#include "particle.h"
 
 using namespace std;
 
-Particle::Particle(double v0, double theta, double x0, double y0, double step);
-{
-    kut = theta*M_PI/180;
-    vx = v0*cos(kut);
-    vy = v0*sin(kut);
-    ti = 0;
-    dt = step;
-    xi = x0;
-    yi = y0;
-}
+Particle::Particle(double v0, double theta, double x0, double y0, double step); //constructor
+    {
+        ti = 0;
+        dt = step;
+        xi = x0;
+        yi = y0;
+        kut = theta*M_PI/180;
+        vx = v0*cos(kut);
+        vy = v0*sin(kut);
+    };
 
+void Particle::evolve()
+    {
+        while (yi >= 0) {
+            vx += 0.;
+            vy += g*dt;
 
+            xi += vx*dt;
+            yi += vy*dt;
 
+            ti += dt;
+        }
+    }
 
+double Particle::range()
+    {
+        evolve();
+        D = xi;
+        return D;
+    }
 
-
-    // iz pythona
-    /*def reset(self):
-        self.v0 = 0 
-        self.theta = 0
-        self.xi = 0
-        self.yi = 0
-        self.dt = 0
-        self.x = []
-        self.y = []
-        self.kut = 0
-        self.v0x = 0
-        self.v0y = 0
-
-    def __move(self):
-        a = 9.81
-        self.xi = self.xi + self.vx*self.dt
-        self.vy = self.vy - a*self.dt
-        self.yi = self.yi + self.vy*self.dt
-        self.x.append(self.xi)
-        self.y.append(self.yi)
-
-    def range(self):
-        while self.yi >= 0:
-            self.__move()
-        return max(self.x)
-
-    def analiticki_domet(self):
-        D = ((self.v0**2)*math.sin(2*self.kut))/9.81
-        return D
-
-    def total_time(self):
-        t = 0
-        while self.yi >= 0:
-            self.__move()
-            t = t + self.dt
-        return t */
+double Particle::time()
+    {
+        evolve();
+        t = ti;
+        return t;
+    }
--- Vjezbe_7/particle.h
+++ Vjezbe_7/particle.h
@@ -2,17 +2,18 @@ class Particle {
     
     private:
 
-        double ti,xi,yi,vx,vy;
+        double ti,xi,yi,vx,vy,kut;  //attributes
         double dt;
         double g = -9.81;
+        double D,t;
 
         void evolve();
 
 
     public:
 
-        Particle(double v0, double theta, double x0, double y0, double step = 0.001);
-        ~Particle();
+        Particle(double v0, double theta, double x0, double y0, double step = 0.001);  //constructor
+        ~Particle();  //destructor
 
         double range();
         double time();
