ffe30b5fab9a78a410ba1db09f0d1c6094190966
Lucija Marevic <lmarevic@pmfst.hr>
2021-04-30 23:00:29 +0200

    nedodvrseno

--- Vjezbe_6/z3.cpp
+++ Vjezbe_6/z3.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <list>
 
-const int n = 5;
-int lista[n] = {1,5,2,4,6};
-
-void ispis_u_intervalu(unsigned a, unsigned b) {
+void ispis_u_intervalu(int lista[], unsigned a, unsigned b) {
+    int n = sizeof(lista);  // tako dobijem broj elemenata
     for(int i = 0; i < n; i++) {
         if (lista[i] >= a && lista[i] <= b) {
             std::cout << lista[i] << " ";
@@ -15,17 +13,28 @@ void ispis_u_intervalu(unsigned a, unsigned b) {
         std::cout << std::endl;
         }   
 
-void reverse_list()  {   // ne valja
-    std::cout << "Obrnuti redosljed glasi: " << std::endl;
-    std::list<int> lista;
-    lista.reverse();
-    for (std::list<int>::iterator it=lista.begin(); it!=lista.end(); ++it)
-        std::cout << ' \t' << *it;
-    std::cout << '\n';
+void obrnuti_redosljed(int lista[])  {
+    int n = sizeof(lista);
+    int lista_1[n] = {};
+    for (int i = 0; i = n-1; i++) {
+        int k = lista[n - i];  // npr. za i = 3, uzet ce 3 element od kraja
+        lista_1[i] = k;  
+        std::cout << lista[i] << " ";
+    }
+    std::cout << std::endl;
+    /* for (int i = 0; i = n-1; i++) {
+        lista_1[i] = lista[i];
+        std::cout << lista_1[i] << " ";
+    } 
+    std::cout << std::endl;  */
 }
 
+//void zamjeni_clanove(lista)
+
 int main() {
-    ispis_u_intervalu(2,7);
-    reverse_list();
+    const int n = 5;
+    int lista[n] = {1,5,2,4,6};
+    ispis_u_intervalu(lista, 2,7);
+    obrnuti_redosljed(lista);
     return 0;
 }
--- Vjezbe_6/z4.cpp
+++ Vjezbe_6/z4.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
-//#include <math.h>
 
-using namespace std;
+void rjesi_sustav(float a1, float b1, float c1, float a2, float b2, float c2) {
+    /* iz prve jednaadzbe dobijemo x = (c1 - b1*y)/a1, 
+    i kad uvrstimo u drugu y(b2 - b1*a2/a1) = c2 - c1*a2/a1 */
+    float k_b = b2 - b1*a2/a1;
+    float k_c = c2 - c1*a2/a1;
+    float y = k_c/k_b;
+    float x = (c1 - b1*y)/a1;
+
+    std::cout << "x = " << x << ", a y = " << y << std::endl;
+}
+
+int main() {
+    rjesi_sustav(1,3,5,2,4,6);
+    return 0;
+}
