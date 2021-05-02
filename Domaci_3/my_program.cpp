65bb37c5430221ef90754b4235d2b8666206715b
Lucija Marevic <lmarevic@pmfst.hr>
2021-05-02 11:39:06 +0200

    udefined reference to HarmonicOscillator

--- Domaci_3/my_program.cpp
+++ Domaci_3/my_program.cpp
@@ -2,8 +2,8 @@
 #include "harm_o.h"
 
 int main() {
-    HarmonicOscillator o1(0.1,5,0,0.5,10);  //(m,k,v0,x0,time,step = 0.001); 
+    HarmonicOscilator o1(0.1,5,0,0.5,10);  //(m,k,v0,x0,time,step = 0.001); 
     //std::cout << "Period iznosi: " << o1.period() << "s." << std::endl;  
     
     return 0;
-    }
+    } 
