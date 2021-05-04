05f0bbe2bcda715d3e99555e6ae89e5b9be996df
Lucija Marevic <lmarevic@pmfst.hr>
2021-05-04 20:58:57 +0200

    dodat datoteku kad skuzis zasto ne rade

--- Domaci_3/zadatak_1.cpp
+++ Domaci_3/zadatak_1.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include "harm_o.h"
+#include "harm_o.h" 
+#include <iostream> 
+#include <fstream>   //za *.txt file
+#include <string>
 
 int main() {
-    HarmonicOscillator o1(0.1,10,0,0.5,5);     //(m,k,v0,x0,time,step)
+    HarmonicOscillator o1(0.1,10,0,0.5,5);   //(m,k,v0,x0,time,step)
     o1.oscillate();
     return 0;
 }
