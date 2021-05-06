52aa098dd24d152c496c9f27d59c41f38a6fc3db
Lucija <lucijamarevic@gmail.com>
2021-05-06 09:42:29 +0200

    nacrtat kao korisnik

--- Vjezbe_8/zadatak_1.py
+++ Vjezbe_8/zadatak_1.py
@@ -1,12 +1,13 @@
 import Projectile as ptl 
-import numpy as np
 
 p = ptl.Projectile()
 
-p.init(10,10,60,0,0,1.2,0.1,0.5,0.01)      #(m,v0,theta,x0,y0,ro,cd,A,dt)
+p.init(10,10,60,0,0,1.2,1,0.5,0.01)      #(m,v0,theta,x0,y0,ro,cd,A,dt)
 p.move()
 p.plot_trajectory()
 p.reset()
 p.init(10,10,60,0,0,1.2,0.1,0.5,0.01)
 p.move_ar()
 p.plot_trajectory()
+
+#nacrtat kao korisnik
