16d8e8cf68a5e9bba52a3ae2919718c373dc1a68
Lucija <lucijamarevic@gmail.com>
2021-05-14 17:01:24 +0200

    drugi zadatak nesto steka

--- Domaci_4/projectile.py
+++ Domaci_4/projectile.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import math 
 
 class Projectile:
@@ -22,10 +23,14 @@ class Projectile:
         self.vy_list.append(self.vy)
         self.x = x0          # za polozaj
         self.y = y0
+        self.x0 = x0
+        self.y0 = y0
         self.x_list.append(self.x)
         self.y_list.append(self.y)
         self.ro = ro         # za akceleraciju
         self.cd = cd
+        self.tijelo = tijelo
+        self.a = a
         if tijelo == "kocka":
             self.A = a**2
         elif tijelo == "kugla":
@@ -120,10 +125,6 @@ class Projectile:
         
         self.ax = self.__ax(self.vx)
 
-        self.x_list.append(self.x)
-        self.vx_list.append(self.vx)
-        self.ax_list.append(self.ax)
-
         # za y komponentu
         k1vy = self.__ay(self.vy)*self.dt 
         k1y = self.vy*self.dt
@@ -138,24 +139,22 @@ class Projectile:
         self.y += (1/6)*(k1y + 2*k2y + 2*k3y + k4y)
 
         self.ay = self.__ay(self.vy)
-            
-        self.y_list.append(self.y)
-        self.vy_list.append(self.vy)
-        self.ay_list.append(self.ay)
 
     def runge_kutta(self):   
         while self.y >= 0:
             self.__runge_kutta()
+            self.x_list.append(self.x)
+            self.vx_list.append(self.vx)
+            self.ax_list.append(self.ax)
+            self.y_list.append(self.y)
+            self.vy_list.append(self.vy)
+            self.ay_list.append(self.ay)
             self.t += self.dt
             self.t_list.append(self.t)
         return self.x_list,self.y_list
         
     def plot_trajectory(self):
-        plt.figure("Graf za trenutno stanje")
         plt.plot(self.x_list,self.y_list)
-        plt.title("x-y graf")
-        plt.xlabel("x[m]")
-        plt.ylabel("y[m]")
         plt.show()
 
     def range(self):
@@ -177,6 +176,75 @@ class Projectile:
     def max_speed(self):
         self.runge_kutta()
         return max(self.v_list)
+    
+    def angle_to_hit_target(self,mx,my,r):
+        self.mx = mx
+        self.my = my
+        self.r = r
+        d_lista = []
+        th_lista = []
+
+        # dio koji racuna kut
+        pogodena = False
+        for self.theta in range(91):
+            self.kut = self.theta*math.pi/180
+            self.vx = self.v0*math.cos(self.kut)
+            self.vy = self.v0*math.sin(self.kut)
+            self.__runge_kutta()
+            D =  math.sqrt((self.mx-self.x)**2 + (self.my-self.y)**2) 
+            if D <= self.r:
+                pogodena = True
+                break
+            else:
+                d_lista.append(D - self.r)
+                th_lista.append(self.theta)
+
+        if pogodena:
+            print("Potreban kut za pogoditi metu je: {}°".format(self.theta))
+            
+            # dio koji crta metu
+            x = []
+            y = []
+            for fi in list(np.linspace(0,360, num = 3600)):    
+                rad = fi*math.pi/180
+                xi = self.mx + self.r*math.cos(rad)
+                x.append(xi)
+                yi = self.my + self.r*math.sin(rad)
+                y.append(yi)
+            plt.plot(x,y)
+
+            # dio koji crta putanju
+            self.init(self.m,self.v0,self.theta,self.x0,self.y0,self.ro,self.cd,self.tijelo,self.a,self.dt)
+            self.runge_kutta()
+            self.plot_trajectory()
+            self.reset()
+        
+        else:      # ode nesto ne valja
+            d = min(d_lista)
+            print("Nije moguce pogoditi metu sa zadanom brzinom.")
+            print("Najmanja udaljenost od mete za zadanu brzinu je: {:.2f}m.".format(d))
+            
+            indeks = d_lista.index(d)
+            theta = th_lista[indeks]
+
+            print("Kut za koji je projektil najblizi meti iznosi: {}°.".format(theta))
+
+            # dio koji crta metu
+            x = []
+            y = []
+            for fi in list(np.linspace(0,360, num = 3600)):    
+                rad = fi*math.pi/180
+                xi = self.mx + self.r*math.cos(rad)
+                x.append(xi)
+                yi = self.my + self.r*math.sin(rad)
+                y.append(yi)
+            plt.plot(x,y)
+
+            # dio koji crta putanju
+            self.init(self.m,self.v0,theta,self.x0,self.y0,self.ro,self.cd,self.tijelo,self.a,self.dt)
+            self.runge_kutta()
+            self.plot_trajectory()
+            self.reset()
 
     #def velocity_to_hit_target(self,mx,my,r):
     #    self.mx = mx
@@ -239,72 +307,5 @@ class Projectile:
 
             # dio koji crta putanju
     #        self.init(v,self.theta,self.x0,self.y0,self.dt )
-    #        self.plot_trajectory()
-    #        self.reset()
-    
-    #def angle_to_hit_target(self,mx,my,r):
-    #    self.mx = mx
-    #    self.my = my
-    #    self.r = r
-    #    d_lista = []
-    #    th_lista = []
-
-        # dio koji racuna kut
-    #    pogodena = False
-    #    for self.theta in range(91):
-    #        self.kut = self.theta*math.pi/180
-    #        self.vx = self.v0*math.cos(self.kut)
-    #        self.vy = self.v0*math.sin(self.kut)
-    #        self.__move()
-    #        D =  math.sqrt((self.mx-self.xi)**2 + (self.my-self.yi)**2) 
-    #        if D <= self.r:
-    #            pogodena = True
-    #            break
-    #        else:
-    #            d_lista.append(D - self.r)
-    #            th_lista.append(self.theta)
-
-    #    if pogodena:
-    #        print("Potreban kut za pogoditi metu je: {}°".format(self.theta))
-            
-            # dio koji crta metu
-    #        x = []
-    #        y = []
-    #        for fi in list(np.linspace(0,360, num = 3600)):    
-    #            rad = fi*math.pi/180
-    #            xi = self.mx + self.r*math.cos(rad)
-    #            x.append(xi)
-    #            yi = self.my + self.r*math.sin(rad)
-    #            y.append(yi)
-    #        plt.plot(x,y)
-
-            # dio koji crta putanju
-    #        self.init(self.v0,self.theta,self.x0,self.y0,self.dt )
-    #        self.plot_trajectory()
-    #        self.reset()
-        
-    #    else:      # ode nesto ne valja
-    #        d = min(d_lista)
-    #        print("Nije moguce pogoditi metu sa zadanom brzinom.")
-    #        print("Najmanja udaljenost od mete za zadanu brzinu je: {:.2f}m.".format(d))
-            
-    #        indeks = d_lista.index(d)
-    #        theta = th_lista[indeks]
-
-            #print(theta)
-
-            # dio koji crta metu
-    #        x = []
-    #        y = []
-    #        for fi in list(np.linspace(0,360, num = 3600)):    
-    #            rad = fi*math.pi/180
-    #            xi = self.mx + self.r*math.cos(rad)
-    #            x.append(xi)
-    #            yi = self.my + self.r*math.sin(rad)
-    #            y.append(yi)
-    #        plt.plot(x,y)
-
-            # dio koji crta putanju
-    #        self.init(self.v0,theta,self.x0,self.y0,self.dt )
     #        self.plot_trajectory()
     #        self.reset()
