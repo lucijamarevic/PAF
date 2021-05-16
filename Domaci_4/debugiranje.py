8823b06e0448b31704c7242accf6b0736f5a652c
Lucija <lucijamarevic@gmail.com>
2021-05-16 23:42:40 +0200

    Domaci_4

--- Domaci_4/debugiranje.py
+++ Domaci_4/debugiranje.py
@@ -1,23 +1,21 @@
 import projectile as ptl
 import matplotlib.pyplot as plt
-
-p1 = ptl.Projectile()
-
-p1.init(10,10,28,0,0,1,2,"kugla",1,0.01)    #(m,v0,theta,x0,y0,ro,cd,tijelo,a,dt)
-x,y = p1.runge_kutta()
-plt.plot(x,y, label = "28°")
+import math
 
 p2 = ptl.Projectile()
 
-p2.init(10,30,18,0,0,1,2,"kugla",1,0.01)    #(m,v0,theta,x0,y0,ro,cd,tijelo,a,dt)
-w,z = p2.runge_kutta()
-plt.plot(w,z, label = "18°")
+p2.init(10,30,21,0,0,1,2,0.1,0.01)    #(m,v0,theta,x0,y0,ro,cd,a,dt,tijelo = "kugla")
+p2.runge_kutta()
+p2.plot_trajectory()
 
-p3 = ptl.Projectile()
+x = []
+y = []
+for fi in range(360):    
+    rad = fi*math.pi/180
+    xi = 6 + 0.5*math.cos(rad)
+    x.append(xi)
+    yi = 2 + 0.5*math.sin(rad)
+    y.append(yi)
 
-p3.init(5,50,7,0,0,1,2,"kugla",1,0.01)    #(m,v0,theta,x0,y0,ro,cd,tijelo,a,dt)
-a,b = p3.runge_kutta()
-plt.plot(a,b, label = "7°")
-
-plt.legend()
-plt.show() 
+plt.plot(x,y)
+plt.show()
