a969f56743610f35bafa1b39872861b5df8c808a
Lucija <lucijamarevic@gmail.com>
2021-05-17 18:24:02 +0200

    provjerit move i plot_trajectory

--- Vjezbe_10/em_field.py
+++ Vjezbe_10/em_field.py
@@ -1,8 +1,9 @@
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
 import numpy as np
 import math
 
 class EmField:
-    #mi0 = 4*math.pi*(10**(-7))
     def __init__(self):
         self.r_lista = []
         self.x_lista = []
@@ -12,37 +13,28 @@ class EmField:
         self.a_lista = []
         self.t_lista = []
 
-    def init(self,m,q,x0,y0,z0,v0x,v0y,v0z,Ex,Ey,Ez,Bx,By,Bz,dt):
+    def init(self,m,q,r,v,E,B,dt):
         self.m = m
         self.q = q
-        self.x = x0
-        self.y = y0
-        self.z = z0
-        self.r = np.array((x0,y0,z0))
+        self.r = r
         self.r_lista.append(self.r)
-        self.x_lista.append(x0)
-        self.y_lista.append(y0)
-        self.z_lista.append(z0)
-        self.vx = v0x
-        self.vy = v0y
-        self.vz = v0z
-        self.v = np.array((v0x,v0y,v0z))
+        self.x_lista.append(self.r[0])
+        self.y_lista.append(self.r[1])
+        self.z_lista.append(self.r[2])
+        self.v = v
         self.v_lista.append(self.v)
-        self.Ex = Ex
-        self.Ey = Ey
-        self.Ez = Ez
-        self.E = np.array((Ex,Ey,Ez))
-        self.Bx = Bx
-        self.By = By
-        self.Bz = Bz
-        self.B = np.array((Bx,By,Bz))
+        self.E = E
+        self.B = B
         self.a = self.__akceleracija(self.v)
         self.a_lista.append(self.a)
-        self.dt = 0
+        self.dt = dt
         self.t = 0
         self.t_lista.append(self.t)
 
-        print(self.a)
+        print("akceleracija: {}".format(self.a))
+        print("brzina: {}".format(self.v))
+        print("polozaj: {}".format(self.r))
+        print("x: {}, y: {}, z: {}".format(self.x_lista,self.y_lista,self.z_lista))
 
     def __akceleracija(self,v):
         return (self.q/self.m) * (np.add(self.E,np.cross(v,self.B)))
@@ -63,4 +55,9 @@ class EmField:
             self.v_lista.append(self.v)
             self.a_lista.append(self.a)
             self.t_lista.append(self.t)
+
+    def plot_trajectory(self):
+        ax = plt.axes(projection = "3d")
+        ax.plot3D(self.x_lista,self.y_lista,self.z_lista, 'green')
+        plt.show()
             
--- Vjezbe_10/zadatak_1.py
+++ Vjezbe_10/zadatak_1.py
@@ -1,4 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import em_field as f
 
 o = f.EmField()
-o.init(1,1,0,0,0,1,1,1,1,1,1,1,2,3,0.01)  #(m,q,x0,y0,z0,v0x,v0y,v0z,Ex,Ey,Ez,Bx,By,Bz,dt)
+o.init(1,1,np.array((0,0,0)),np.array((0.1,0.1,0.1)),np.array((0,0,0)),np.array((0,0,1)),0.01)  #(m,q,r,v,E,B,dt)
+#o.move(0.1)
+
+#print("Akceleracije: ")
+#print(o.a_lista)
