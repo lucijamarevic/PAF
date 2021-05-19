857238b7f3c3ae8c723abaf67bcceaffc5b14d4d
Lucija <lucijamarevic@gmail.com>
2021-05-19 20:06:21 +0200

    Vjezbe_10

--- Vjezbe_10/em_field.py
+++ Vjezbe_10/em_field.py
@@ -5,59 +5,55 @@ import math
 
 class EmField:
     def __init__(self):
-        self.r_lista = []
         self.x_lista = []
         self.y_lista = []
         self.z_lista = []
-        self.v_lista = []
-        self.a_lista = []
-        self.t_lista = []
 
-    def init(self,m,q,r,v,E,B,dt):
+    def init(self,m,q,r,v,E,B,dt):    #sve dobro
         self.m = m
         self.q = q
         self.r = r
-        self.r_lista.append(self.r)
         self.x_lista.append(self.r[0])
         self.y_lista.append(self.r[1])
         self.z_lista.append(self.r[2])
         self.v = v
-        self.v_lista.append(self.v)
         self.E = E
         self.B = B
         self.a = self.__akceleracija(self.v)
-        self.a_lista.append(self.a)
         self.dt = dt
         self.t = 0
-        self.t_lista.append(self.t)
 
-        print("akceleracija: {}".format(self.a))
-        print("brzina: {}".format(self.v))
-        print("polozaj: {}".format(self.r))
-        print("x: {}, y: {}, z: {}".format(self.x_lista,self.y_lista,self.z_lista))
+    def reset(self):
+        self.m = 0
+        self.q = 0
+        self.r = 0
+        self.x_lista = []
+        self.y_lista = []
+        self.z_lista = []
+        self.v = 0
+        self.E = 0
+        self.B = 0
+        self.a = 0
+        self.dt = 0
+        self.t = 0
 
     def __akceleracija(self,v):
         return (self.q/self.m) * (np.add(self.E,np.cross(v,self.B)))
 
     def __move(self):
-        self.v += self.a*self.dt
-        self.r += self.v*self.dt
+        self.v = np.add(self.v,self.a*self.dt)
+        self.r = np.add(self.r,self.v*self.dt)
         self.a = self.__akceleracija(self.v)
         self.t += self.dt
     
     def move(self,t):
         while self.t <= t:
-            self.__move
-            self.r_lista.append(self.r)
+            self.__move()
             self.x_lista.append(self.r[0])
             self.y_lista.append(self.r[1])
             self.z_lista.append(self.r[2])
-            self.v_lista.append(self.v)
-            self.a_lista.append(self.a)
-            self.t_lista.append(self.t)
 
     def plot_trajectory(self):
         ax = plt.axes(projection = "3d")
-        ax.plot3D(self.x_lista,self.y_lista,self.z_lista, 'green')
-        plt.show()
-            
+        ax.plot3D(self.x_lista,self.y_lista,self.z_lista)
+        plt.show()            
--- Vjezbe_10/zadatak_1.py
+++ Vjezbe_10/zadatak_1.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import em_field as f
+import em_field as field
 
-o = f.EmField()
-o.init(1,1,np.array((0,0,0)),np.array((0.1,0.1,0.1)),np.array((0,0,0)),np.array((0,0,1)),0.01)  #(m,q,r,v,E,B,dt)
-#o.move(0.1)
+e = field.EmField()
+#(m,q,r,v,E,B,dt)
+e.init(1,-1,np.array([0,0,0]),np.array([0.1,0.1,0.1]),np.array([0,0,0]),np.array([0,0,1]),0.01)  
+e.move(20)
 
-#print("Akceleracije: ")
-#print(o.a_lista)
+p = field.EmField()
+#(m,q,r,v,E,B,dt)
+p.init(1,1,np.array([0,0,0]),np.array([0.1,0.1,0.1]),np.array([0,0,0]),np.array([0,0,1]),0.01)  
+p.move(20)
+
+ax = plt.axes(projection = "3d")
+ax.plot3D(e.x_lista,e.y_lista,e.z_lista, c = "blue", label = "elektron")
+ax.plot3D(p.x_lista,p.y_lista,p.z_lista, c = "red", label = "pozitron")
+plt.legend()
+plt.show()
