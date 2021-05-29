8e13c4d41b94e8ac6fe921bec6ee283565c95901
Lucija <lucijamarevic@gmail.com>
2021-05-29 23:06:47 +0200

    progress

--- Vjezbe_12/gravity.py
+++ Vjezbe_12/gravity.py
@@ -14,32 +14,51 @@ class Gravity:
         self.planeti = []
 
     def add_planet(self,planet):
-        self.planeti.append(planet)   #spreman planete u listu
+        self.planeti.append(planet)
 
-    #ovo dalje napola valja ahahah
     def __interact(self):
-        for p in self.list:
+        a = []
+        for p in self.planeti:
             self.m = p[0]
             self.v = p[1]
             self.r = p[2]
-        
-            ds = math.sqrt((self.rs[0]-self.r[0])**2 + (self.rs[1]-self.r[1])**2)
-            self.as = -self.G * self.m/(ds**3) * np.subtract(self.rs,self.r)
-            self.vs = np.add(self.vs,self.as*self.dt)
-            self.rs = np.add(self.rs,self.vs*self.dt)
-            
-            d = math.sqrt((self.r[0]-self.rs[0])**2 + (self.r[1]-self.rs[1])**2)
-            self.a = -self.G * self.ms/(d**3) * np.subtract(self.r,self.rs)
+            i = self.planeti.index(p)
+            del self.planeti[p]
+
+            for p2 in self.planeti:
+                self.m2 = p2[0]
+                self.v2 = p2[1]
+                self.r2 = p2[2]
+
+                d = math.sqrt((self.r[0]-self.r2[0])**2 + (self.r[1]-self.r2[1])**2)
+                self.ai = -self.G * self.m2/(d**3) * np.subtract(self.r,self.r2)
+
+                a.append(self.ai)
+
+            self.a = 0
+            for i in range(0,len(a),2):
+                self.a = np.add(self.a,np.add(a[i],a[i+1]))
             self.v = np.add(self.v,self.a*self.dt)
             self.r = np.add(self.r,self.v*self.dt)
 
-    def interact(self,t):
-        while self.t <= t:
-            self.__interact()
-            #self.x1_list.append(self.r1[0])
-            #self.y1_list.append(self.r1[1])
-            #self.x2_list.append(self.r2[0])
-            #self.y2_list.append(self.r2[1])
-            self.t += self.dt
+    #ovo dalje napola valja ahahah
+    #def __interact(self):
+        #for p in self.list:
+        #    self.m = p[0]
+        #    self.v = p[1]
+        #    self.r = p[2]
+        
+        #    ds = math.sqrt((self.rs[0]-self.r[0])**2 + (self.rs[1]-self.r[1])**2)
+        #    self.as = -self.G * self.m/(ds**3) * np.subtract(self.rs,self.r)
+        #    self.vs = np.add(self.vs,self.as*self.dt)
+        #    self.rs = np.add(self.rs,self.vs*self.dt)
+            
+        #    d = math.sqrt((self.r[0]-self.rs[0])**2 + (self.r[1]-self.rs[1])**2)
+        #    self.a = -self.G * self.ms/(d**3) * np.subtract(self.r,self.rs)
+        #    self.v = np.add(self.v,self.a*self.dt)
+        #    self.r = np.add(self.r,self.v*self.dt)
 
-        #return self.x1_list, self.y1_list, self.x2_list , self.y2_list
+    #def interact(self,t):
+    #    while self.t <= t:
+    #        self.__interact()
+    #        self.t += self.d
