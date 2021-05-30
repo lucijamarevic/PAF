4ea3cb315c49dc6735f25f2dce6e381795bddb4c
Lucija <lucijamarevic@gmail.com>
2021-05-30 22:17:49 +0200

    neki error, vjerojatno s listama

--- Vjezbe_12/gravity.py
+++ Vjezbe_12/gravity.py
@@ -24,26 +24,25 @@ class Universe:
     #ode dalje je negdi greska
 
     def __interact(self,m,v,r):
-        a = []
         self.x = []
         self.y = []
         self.m = m
         self.v = v
         self.r = r
+        self.a = 0
 
         for p2 in self.planeti:
             self.m2 = p2[0]
             self.v2 = p2[1]
             self.r2 = p2[2]
 
-            d = math.sqrt((self.r[0]-self.r2[0])**2 + (self.r[1]-self.r2[1])**2)
-            self.ai = -self.G * self.m2/(d**3) * np.subtract(self.r,self.r2)
-
-            a.append(self.ai)
-
-        self.a = 0
-        for i in range(0,len(a)-1,2):
-                self.a = np.add(self.a,np.add(a[i],a[i+1]))
+            if self.m2 == self.m:
+                pass
+            
+            else:
+                d = math.sqrt((self.r[0]-self.r2[0])**2 + (self.r[1]-self.r2[1])**2)
+                a = -self.G * self.m2/(d**3) * np.subtract(self.r,self.r2)
+                self.a = np.add(self.a,a)
 
         self.v = np.add(self.v,self.a*self.dt)
         self.r = np.add(self.r,self.v*self.dt)
@@ -56,8 +55,6 @@ class Universe:
             m = p[0]
             v = p[1]
             r = p[2]
-            i = self.planeti.index(p)
-            del self.planeti[i]
 
             while self.t <= t:
                 self.__interact(m,v,r)
