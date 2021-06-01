2ecccbb195d724a01a51a5413c6ce11fbdf98275
Lucija <lucijamarevic@gmail.com>
2021-06-01 15:17:30 +0200

    jos uvik greska

--- Vjezbe_12/gravity.py
+++ Vjezbe_12/gravity.py
@@ -2,14 +2,14 @@ import numpy as np
 import math
 
 class Planet: 
-    def __init__(self,m,v,r):     #radi 
+    def __init__(self,m,v,r): 
         self.list = [m,v,r]
 
-    def return_list(self):     #radi
+    def return_list(self):
         return self.list
 
 class Universe:
-    def __init__(self,dt = 60*6*24):     #radi
+    def __init__(self,dt = 60*6*24):
         self.G =  6.67408 * (10**(-11))
         self.t = 0
         self.dt = dt
@@ -17,11 +17,29 @@ class Universe:
         self.x_list = []
         self.y_list = []
 
-    def add_planet(self,planet):     #radi
+    def add_planet(self,planet): 
         self.planeti.append(planet)
 
+    def __reset_1(self):
+        self.m = 0
+        self.r = 0
+        self.v = 0
+        self.a = 0
+        self.t = 0
+        self.x = []
+        self.y = []
+
+    def __reset_2(self):
+        self.m2 = 0
+        self.r2 = 0
+        self.v2 = 0
 
     #ode dalje je negdi greska
+
+    def force(self,m1,m2,r1,r2):
+        d = math.sqrt((r1[0]-r2[0])**2 + (r1[1]-r2[1])**2)
+        sila = -self.G * m1*m2/(d**3) * np.subtract(r1,r2)
+        return sila
 
     def __interact(self,m,v,r):
         self.x = []
@@ -29,7 +47,7 @@ class Universe:
         self.m = m
         self.v = v
         self.r = r
-        self.a = 0
+        self.F = np.array((0,0))
 
         for p2 in self.planeti:
             self.m2 = p2[0]
@@ -37,13 +55,14 @@ class Universe:
             self.r2 = p2[2]
 
             if self.m2 == self.m:
-                pass
+                F = np.array((0,0))
             
             else:
-                d = math.sqrt((self.r[0]-self.r2[0])**2 + (self.r[1]-self.r2[1])**2)
-                a = -self.G * self.m2/(d**3) * np.subtract(self.r,self.r2)
-                self.a = np.add(self.a,a)
-
+                F = self.force(self.m,self.m2,self.r,self.r2)
+                
+            self.F = np.add(self.F,F)
+        
+        self.a = self.F/self.m
         self.v = np.add(self.v,self.a*self.dt)
         self.r = np.add(self.r,self.v*self.dt)
 
@@ -58,9 +77,9 @@ class Universe:
 
             while self.t <= t:
                 self.__interact(m,v,r)
+                self.__reset_2()
                 self.t += self.dt
 
             self.x_list.append(self.x)
             self.y_list.append(self.y)
-            self.x.clear()
-            self.y.clear()
+            self.__reset_1()
