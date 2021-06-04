c52e76839541831cdf4d1344cc05e0d01f86e3a3
Lucija <lucijamarevic@gmail.com>
2021-06-04 16:06:34 +0200

    napredak

--- Vjezbe_12/gravity.py
+++ Vjezbe_12/gravity.py
@@ -10,6 +10,7 @@ class Planet:
         self.v = v
         self.r0 = r
         self.r = r
+        self.F = np.array((0,0))
         self.x_list = []
         self.y_list = []
         self.x_list.append(self.r[0])
@@ -29,24 +30,18 @@ class Universe:
     def add_planet(self,planet):
         self.planets.append(planet)
 
-    def force(self,m1,m2,r1,r2):
-        d = math.sqrt((r1[0]-r2[0])**2 + (r1[1]-r2[1])**2)
-        F = -self.G * m1 * m2/(d**3) * np.subtract(r1,r2)
+    def force(self,pl1,pl2):
+        d = math.sqrt((pl1.r[0]-pl2.r[0])**2 + (pl1.r[1]-pl2.r[1])**2)
+        F = -self.G * pl1.m * pl2.m/(d**3) * np.subtract(pl1.r,pl2.r)
         return F
 
     def __interact(self):
         for p in self.planets:
-            F = np.array((0,0))
             for p2 in self.planets:
-                if p2 == p:
-                    F = 0
-                else:
-                    F = np.add(F,self.force(p.m,p2.m,p.r,p2.r))
-                    #p2.a = F/p2.m
-                    #p2.v = np.add(p2.v,p2.a*self.dt)
-                    #p2.r = np.add(p2.r,p2.v*self.dt)
-            p.F = F
-            p.a = p.F/p.m
+                if p2 != p:
+                    F = self.force(p,p2)
+                    p.F = np.add(p.F,F)
+                    p.a = p.F/p.m    
             p.v = np.add(p.v,p.a * self.dt)
             p.r = np.add(p.r,p.v * self.dt)
             p.x_list.append(p.r[0])
