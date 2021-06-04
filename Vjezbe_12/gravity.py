1e104cfc2c10c136d178c6b7c0bc9db1ad0880ee
Lucija <lucijamarevic@gmail.com>
2021-06-04 16:44:32 +0200

    Vjezbe_12

--- Vjezbe_12/gravity.py
+++ Vjezbe_12/gravity.py
@@ -10,7 +10,6 @@ class Planet:
         self.v = v
         self.r0 = r
         self.r = r
-        self.F = np.array((0,0))
         self.x_list = []
         self.y_list = []
         self.x_list.append(self.r[0])
@@ -30,18 +29,18 @@ class Universe:
     def add_planet(self,planet):
         self.planets.append(planet)
 
-    def force(self,pl1,pl2):
+    def __acc(self,pl1,pl2):
         d = math.sqrt((pl1.r[0]-pl2.r[0])**2 + (pl1.r[1]-pl2.r[1])**2)
-        F = -self.G * pl1.m * pl2.m/(d**3) * np.subtract(pl1.r,pl2.r)
-        return F
+        a = -self.G * pl2.m/(d**3) * np.subtract(pl1.r,pl2.r)
+        return a
 
     def __interact(self):
         for p in self.planets:
+            p.a = np.array((0,0))
             for p2 in self.planets:
                 if p2 != p:
-                    F = self.force(p,p2)
-                    p.F = np.add(p.F,F)
-                    p.a = p.F/p.m    
+                    a = self.__acc(p,p2)
+                    p.a = np.add(p.a,a)    
             p.v = np.add(p.v,p.a * self.dt)
             p.r = np.add(p.r,p.v * self.dt)
             p.x_list.append(p.r[0])
--- Vjezbe_12/zadatak_2.py
+++ Vjezbe_12/zadatak_2.py
@@ -1,6 +1,7 @@
 import gravity as g
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 au = 1.486*(10**11)
 day = 60*60*24
@@ -20,12 +21,13 @@ p.add_planet(Earth)
 p.add_planet(Mars)
 #print(p.planets) sprema ih u listu kao objekte iz klase Planet
 
-p.interact(year)
+p.interact(5*year)
 
-plt.plot(Sun.x_list,Sun.y_list, c = Sun.color, label = Sun.name) #linewidth = 5
+plt.figure("Solar system", figsize = (8,8))
+plt.plot(Sun.x_list,Sun.y_list, c = Sun.color, label = Sun.name, linewidth = 2)
 plt.plot(Mercury.x_list,Mercury.y_list, c = Mercury.color, label = Mercury.name)
 plt.plot(Venus.x_list,Venus.y_list, c = Venus.color, label = Venus.name)
 plt.plot(Earth.x_list,Earth.y_list, c = Earth.color, label = Earth.name)
 plt.plot(Mars.x_list,Mars.y_list, c = Mars.color, label = Mars.name)
-plt.legend()
+plt.legend(loc = "upper right")
 plt.show()
